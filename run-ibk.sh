b01fa87a9d32c25d35dc8efe819024b106aaaab8
Jesse Rosalia <thejenix@gmail.com>
2013-01-27 22:21:46 +0000

    Initial scripts: kNN (IBk), Decision Tree (J48), Multilayer Perceptron (mlp). Script to run a bunch of different MLP test cases

---
+++ run-ibk.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+
+weka_dir=weka-3-6-9
+java -cp $weka_dir/weka.jar weka.classifiers.lazy.IBk -K $2 -W 0 -A "weka.core.neighboursearch.LinearNNSearch -A \"weka.core.EuclideanDistance -R first-last\"" -t $weka_dir/data/$1.arff -i -x 10 > $3
