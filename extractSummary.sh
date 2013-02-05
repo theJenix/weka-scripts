62b570f10c97f296232201411f143be42f1b198a
theJenix <jesse.rosalia@gatech.edu>
2013-02-05 05:41:05 +0000

    Added reversed segmentation dataset (for playing with more training/less testing data) Modified extractSummary and run-all-j48.sh to extract meaningful fields in summary file (for better processing in excel)

--- extractSummary.sh
+++ extractSummary.sh
@@ -3,5 +3,4 @@
 # Extract the summary data (Correctly/Incorrectly classified instances, statistics, etc) from one or more output files.  This
 # will replace spaces that separate the fields with commas, for easy processing in excel
 #
-echo ${@}
-grep -A7 'Correctly' ${@} | sed -e 's/\s\{2,\}/,/g' | sed -e 's/^--$//g'
+grep -A7 'Correctly' ${@} | sed -e 's/\s\{2,\}/,/g' | sed -e 's/^--$//g' | sed -e 's/t[-:]/t,/g'
